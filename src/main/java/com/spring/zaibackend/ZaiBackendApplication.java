bb89866c638c7a59dc73bf5cf1c8e67defa47b5c
Piotr Orczykowski <228820@edu.p.lodz.pl>
2023-01-05 20:15:23 +0100

    Initial project

---
+++ src/main/java/com/spring/zaibackend/ZaiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.spring.zaibackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZaiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZaiBackendApplication.class, args);
+	}
+
+}
